048c4a693f2452929f10db01d6ee84f9f6b35b54
1289746201 <yuethanhaonan@outlook.com>
2023-02-02 15:54:47 +0800

    字符串的扩展

---
+++ 3.字符串的扩展/2.字符串的遍历器接口.js
@@ -0,0 +1,24 @@
+// 字符串的遍历器接口
+// ES6 为字符串添加了遍历器接口（详见《Iterator》一章），使得字符串可以被for...of循环遍历。
+
+// for (let codePoint of 'foo') {
+//   console.log(codePoint)
+// }
+// // "f"
+// // "o"
+// // "o"
+// 除了遍历字符串，这个遍历器最大的优点是可以识别大于0xFFFF的码点，传统的for循环无法识别这样的码点。
+
+// let text = String.fromCodePoint(0x20BB7);
+
+// for (let i = 0; i < text.length; i++) {
+//   console.log(text[i]);
+// }
+// // " "
+// // " "
+
+// for (let i of text) {
+//   console.log(i);
+// }
+// // "𠮷"
+// 上面代码中，字符串text只有一个字符，但是for循环会认为它包含两个字符（都不可打印），而for...of循环会正确识别出这一个字符。
